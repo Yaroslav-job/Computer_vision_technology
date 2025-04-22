afe10d66604ba7c0c71959a596d66c7613b0d79f
Yaroslav-job <sarnatsky.yaroslav@gmail.com>
2025-04-22 19:50:53 +0300

    Add files via upload

---
+++ src/test/java/org/example/ImageAPITest.java
@@ -0,0 +1,50 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.Test;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+
+public class ImageAPITest {
+
+    @Test
+    public void testOpenCVInitialization() {
+        try {
+            ImageAPI api = new ImageAPI();
+            String os = api.getOperatingSystemType().name();
+            String version = org.opencv.core.Core.getVersionString();
+
+            System.out.println("OS version - " + os);
+            System.out.println("Open CV version - " + version);
+
+            assertNotNull(version, "OpenCV version should not be null");
+            assertFalse(version.isEmpty(), "OpenCV version should not be empty");
+        } catch (Exception e) {
+            fail("OpenCV initialization failed: " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testFloodFill() throws Exception {
+        ImageAPI api = new ImageAPI();
+        api.floodFillImage("/home/ya/computer_vision_technology/lab_5/Images/1/image.jpg", new Point(0, 0), new Scalar(0, 255, 0), null, null);
+        // Проверить результат вручную
+    }
+
+    @Test
+    public void testPyramidOperations() throws Exception {
+        ImageAPI api = new ImageAPI();
+        api.pyramidOperations("/home/ya/computer_vision_technology/lab_5/Images/1/image.jpg", 2, true); // Понижение
+        api.pyramidOperations("/home/ya/computer_vision_technology/lab_5/Images/1/image.jpg", 2, false); // Повышение
+        // Проверить результат вручную
+    }
+
+    @Test
+    public void testIdentifyRectangles() throws Exception {
+        ImageAPI api = new ImageAPI();
+        api.identifyRectangles("/home/ya/computer_vision_technology/lab_5/Images/1/image.jpg", 100, 50);
+        // Проверить результат вручную
+    }
+}
